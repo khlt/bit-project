f366d69e80c41f1cfa8408fa3a600b1a7b96320b
khlt <kh356113@gmail.com>
2022-11-10 23:39:52 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,91 @@
+// 헤더
+
+let header = document.querySelector(".mainList");
+let headerHeight = header.offsetHeight;
+
+window.onscroll = function () {
+  let windowTop = window.scrollY;
+  // 스크롤 세로값이 헤더높이보다 크거나 같으면
+  // 헤더에 클래스 'drop'을 추가한다
+  if (windowTop >= headerHeight * 7) {
+    header.classList.add("drop");
+  }
+  // 아니면 클래스 'drop'을 제거
+  else {
+    header.classList.remove("drop");
+  }
+};
+
+//슬라이드
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+//
+
+// Scroll Animation (sa, 스크롤 애니메이션)
+const saDefaultMargin = 300;
+let saTriggerMargin = 0;
+let saTriggerHeight = 0;
+const saElementList = document.querySelectorAll(".sa");
+
+const saFunc = function () {
+  for (const element of saElementList) {
+    if (!element.classList.contains("show")) {
+      if (element.dataset.saMargin) {
+        saTriggerMargin = parseInt(element.dataset.saMargin);
+      } else {
+        saTriggerMargin = saDefaultMargin;
+      }
+
+      if (element.dataset.saTrigger) {
+        saTriggerHeight =
+          document
+            .querySelector(element.dataset.saTrigger)
+            .getBoundingClientRect().top + saTriggerMargin;
+      } else {
+        saTriggerHeight = element.getBoundingClientRect().top + saTriggerMargin;
+      }
+
+      if (window.innerHeight > saTriggerHeight) {
+        let delay = element.dataset.saDelay ? element.dataset.saDelay : 0;
+        setTimeout(function () {
+          element.classList.add("show");
+        }, delay);
+      }
+    }
+  }
+};
+
+window.addEventListener("load", saFunc);
+window.addEventListener("scroll", saFunc);
