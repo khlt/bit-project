adbeba71938ca443e3bd9a6f2c920f0ca71b61f6
khlt <kh356113@gmail.com>
2022-11-11 15:31:05 +0900

    second commit

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ window.onscroll = function () {
   let windowTop = window.scrollY;
   // 스크롤 세로값이 헤더높이보다 크거나 같으면
   // 헤더에 클래스 'drop'을 추가한다
-  if (windowTop >= headerHeight * 7) {
+  if (windowTop >= headerHeight * 6) {
     header.classList.add("drop");
   }
   // 아니면 클래스 'drop'을 제거
@@ -89,3 +89,51 @@ const saFunc = function () {
 
 window.addEventListener("load", saFunc);
 window.addEventListener("scroll", saFunc);
+
+// background1~4
+
+let mainText = document.querySelector(".background1 h1");
+let mainText2 = document.querySelector(".background1 p");
+let mainText3 = document.querySelector(".background2 h1");
+let mainText4 = document.querySelector(".background2 div:last-child");
+
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  console.log("value");
+
+  if (value > 600) {
+    mainText.style.animation = "disappear 1s forwards";
+  } else {
+    mainText.style.animation = `slide_background 1s`;
+  }
+});
+
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+
+  if (value > 700) {
+    mainText2.style.animation = "disappear2 1s forwards";
+  } else {
+    mainText2.style.animation = `slide_background2 1s`;
+  }
+});
+
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+
+  if (value > 1900) {
+    mainText3.style.animation = "slide_background3 1.5s";
+  } else {
+    mainText3.style.animation = `disappear3 1s forwards`;
+  }
+});
+
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+
+  if (value > 2000) {
+    mainText4.style.animation = "slide_background4 2s";
+  } else {
+    mainText4.style.animation = `disappear4 1s forwards`;
+  }
+});
